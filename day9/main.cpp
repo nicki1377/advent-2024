52b51e05d7f269b6b9a63114c73634b3dc083c78
nicki1377 <76948246+nicki1377@users.noreply.github.com>
2024-12-10 21:00:56 -0600

    refactor + part 2

--- day9/main.cpp
+++ day9/main.cpp
@@ -37,33 +37,118 @@ std::vector<int> parse(std::string path) {
     return disk;
 }
 
-void compactDisk(std::vector<int>& disk) {
-    int i = 0;
-    int j = disk.size() - 1;
+void printDisk(std::vector<int>& disk) {
+    for (int i = 0; i < disk.size(); i++) {
+        if (disk[i] != -1) {
+            printf(" ");
+        }
+        printf("%d ", disk[i]);
+    }
+    printf("\n");
+}
 
+int getSpaceSize(std::vector<int>& disk, int startIndex) {
+    if (startIndex == -1) {
+        return -1;
+    }
+
+    int size = 0;
+    int end = disk.size();
+    int i = startIndex;
+    while (i < end && disk[i] == -1) {
+        size++;
+        i++;
+    }
+    return size;
+}
+
+int getFileSize(std::vector<int>& disk, int lastIndex) {
+    int val = disk[lastIndex];
+
+    int size = 0;
+    int i = lastIndex;
+    while (i >= 0 && disk[i] == val) {
+        size++;
+        i--;
+    }
+    return size;
+}
+
+int findNextSpace(std::vector<int>& disk, int index) {
+    int end = disk.size();
+    while (index < end && disk[index] != -1) {
+        index++;
+    }
+    if (index == end) {
+        return -1;
+    }
+    return index;
+}
+
+int findPrevFile(std::vector<int>& disk, int index) {
+    while (index >= 0 && disk[index] == -1) {
+        index--;
+    }
+    return index;
+}
+
+void compactDisk(std::vector<int>& disk) {
     if (disk.size() == 0) {
         std::cerr << "Disk empty, cannot compact" << std::endl;
         exit(2);
     }
-    // find first non-empty block from end
-    // and first empty block from start
-    while (disk[j] == -1) {
-        j--;
-    }
-    while (disk[i] > -1) {
-        i++;
-    }
+
+    int i = findNextSpace(disk, 0);
+    int j = findPrevFile(disk, disk.size() - 1);
+    
     while (i < j) {
         disk[i] = disk[j];
         disk[j] = -1;
 
-        // find next non-empty block from end
-        while(disk[j] == -1) {
-            j--;
+        j = findPrevFile(disk, j);
+        i = findNextSpace(disk, i);
+    }
+}
+
+void compactByFile(std::vector<int>& disk) {
+    if (disk.size() == 0) {
+        std::cerr << "Disk empty, cannot compact" << std::endl;
+        exit(2);
+    }
+
+    // i will represent first block of empty space
+    // j will represent last block of file
+    int j = findPrevFile(disk, disk.size() - 1);
+    int fileSize = getFileSize(disk, j);
+    int currentFile = disk[j];
+
+    while (j >= 0) {
+        int i = findNextSpace(disk, 0);
+        int spaceSize = getSpaceSize(disk, i);
+
+        // findNextSpace returns -1 when there is no next space
+        // spaceSize will still run
+        while (spaceSize < fileSize && i != -1 && i < j) {
+            i = findNextSpace(disk, i + spaceSize);
+            spaceSize = getSpaceSize(disk, i);
         }
-        // find next empty block from start
-        while (disk[i] > -1) {
-            i++;
+
+        // only perform moving operations if there is a valid space
+        // otherwise skip this step altogether
+        if (spaceSize >= fileSize && i < j) {
+            for (int k = 0; k < fileSize; k++) {
+                disk[i + k] = disk[j - k];
+                disk[j - k] = -1;
+            }
+        }
+
+        j = findPrevFile(disk, j - fileSize);
+        fileSize = getFileSize(disk, j);
+        currentFile--;
+        // ensure files that have already been moved are not moved again
+        while (j >= 0 && disk[j] != currentFile) {
+            j = findPrevFile(disk, j - fileSize);
+            fileSize = getFileSize(disk, j);
         }
     }
 }
@@ -84,13 +169,20 @@ int main() {
     std::string path = "input";
     std::vector<int> disk = parse(path);
     printf("parse done. disk size: %llu\n", disk.size());
-    compactDisk(disk);
+    //printDisk(disk);
+
+    // part 1
+    // compactDisk(disk);
+    // part 2
+    compactByFile(disk);
     printf("compact done. disk size: %llu\n", disk.size());
 
     // this took me an embarrassingly *long* amount of time to figure out
     // python dynamic int sizing would have saved me 😞
     long long checksum = getChecksum(disk);
-    
+
+    //printDisk(disk);
+
     std::printf("checksum done\n");
     std::printf("%lld\n", checksum);
     return 0;
